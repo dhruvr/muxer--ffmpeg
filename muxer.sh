4541cd4aab30d594888878cb71b89446b3ffdb22
dhruv <dhruvr@exzeo.com>
2017-06-24 01:08:31 +0530

    added validations and exit code

--- muxer.sh
+++ muxer.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
     if [ -z '$1' ]; then
     echo "----------First parameter that is the video path is missing--------";
-    exit
+    exit 1
   fi
 
   if [ -z '$2' ]; then
     echo "----------First parameter that is the audio path is missing--------";
-    exit
+    exit 1
   fi
 
 
   if [ -z '$3' ]; then
     echo "----------First parameter that is the output path is missing--------";
-    exit
+    exit 1
   fi
 
+  if [ ! -f $1 ]; then
+    echo "-------------Video file doest not exist localtion $1-----------------";
+    exit 1
+  fi
 
- echo "inputvide : $1 , input audio: $2; output: $3"
+  if [ ! -f $2 ]; then
+    echo "-------------audio file doest not exist localtion $1-----------------";
+    exit 1
+  fi
 
-  ffmpeg -i  $1 -i $2  -c:v copy -c:a aac -strict experimental -y $3
+ echo "inputvideo : $1 , input audio: $2; output: $3"
 
-echo "-------------------------all processing done: $3------------------------------"
+  ffmpeg -i  $1 -i $2  -c:v copy -c:a aac -strict experimental -y $3 -v error
+
+  RESULT=$?
+
+  if [ "$RESULT" -eq 0 ]
+  then
+    echo "-------------------------all processing done: $3------------------------------"
+  else
+    echo "-------------------Failure processsing video error:Code $?--------------------"
+  fi
+
+exit $RESULT
